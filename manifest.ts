6b1b12f51691c1acf90b9d2b39c6a58dbe81bb6e
Viraj Kaushalye <virajkaush@gmail.com>
2023-05-05 15:22:28 +0530

    start dev

--- manifest.ts
+++ manifest.ts
@@ -8,37 +8,42 @@ const manifest: chrome.runtime.ManifestV3 = {
   name: packageJson.name,
   version: packageJson.version,
   description: packageJson.description,
-  options_page: "src/pages/options/index.html",
+  // options_page: "src/pages/options/index.html",
   background: {
     service_worker: "src/pages/background/index.js",
     type: "module",
   },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icon-34.png",
+    default_icon: "icon-32.png",
   },
-  chrome_url_overrides: {
-    newtab: "src/pages/newtab/index.html",
-  },
+  // chrome_url_overrides: {
+  //   newtab: "src/pages/newtab/index.html",
+  // },
   icons: {
+    "16": "icon-16.png",
+    "32": "icon-32.png",
+    "48": "icon-48.png",
     "128": "icon-128.png",
   },
   content_scripts: [
-    {
-      matches: ["http://*/*", "https://*/*", "<all_urls>"],
-      js: ["src/pages/content/index.js"],
-      // KEY for cache invalidation
-      css: ["assets/css/contentStyle<KEY>.chunk.css"],
-    },
+    // {
+    //   matches: ["http://*/*", "https://*/*", "<all_urls>"],
+    //   js: ["src/pages/content/index.js"],
+    //   // KEY for cache invalidation
+    //   css: ["assets/css/contentStyle<KEY>.chunk.css"],
+    // },
   ],
-  devtools_page: "src/pages/devtools/index.html",
+  // devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
         "assets/js/*.js",
         "assets/css/*.css",
         "icon-128.png",
-        "icon-34.png",
+        "icon-48.png",
+        "icon-32.png",
+        "icon-16.png",
       ],
       matches: ["*://*/*"],
     },
